e7c6838e49c0a721e65b7358e10d1ecac9dbac31
Darian Lee <darianlmj@gmail.com>
2022-02-27 01:13:12 +1100

    Feature: made footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div id='footer'>
+        Made with ♡ by Darian in 2022
+    </div>
+  )
+}
+
+export default Footer
