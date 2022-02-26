cc784f19850c489f358b7323f3be294583f72559
Darian Lee <darianlmj@gmail.com>
2022-02-27 01:18:58 +1100

    Feature: made a page for CTF stuff

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/HomePage';
 import NavigationBar from './components/NavigationBar/NavigationBar';
+import Footer from './components/Footer/Footer';
+import AboutCtf from './pages/AboutCtf/AboutCtf';
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       <NavigationBar/>
       <Switch>
         <Route path='/' component={HomePage} exact/>
+        <Route path='/post/3' component={AboutCtf} exact/>
       </Switch>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/pages/AboutCtf/AboutCtf.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container } from '@mui/material';
+
+import './AboutCtf.css';
+
+const AboutCtf = () => {
+  return (
+    <Container maxWidth='md'>
+      <div id='about-ctf'>
+          <h1>Web Application</h1>
+          <br></br>
+          <h1>Reverse Engineering</h1>
+          <br></br>
+          <h1>Cryptography</h1>
+          <br></br>
+          <h1>Misc</h1>
+          <br></br>
+          <h1>Pwn</h1>
+          <br></br>
+      </div>
+    </Container>
+  )
+}
+
+export default AboutCtf
